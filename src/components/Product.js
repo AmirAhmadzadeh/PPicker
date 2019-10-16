bee1ffedf0b1e3b211d540230d7217c3fb448a11
AmirAhmadzadeh <ahamadzadeh1378@gmail.com>
2019-10-15 21:17:50 -0400

    adding some code !!

---
+++ src/components/Product.js
@@ -0,0 +1,62 @@
+
+import React, { useState, useEffect } from 'react';
+import data from './data';
+import initProdcus from './initProducts' ; 
+
+function Products() {
+    const [products, setProducts] = useState([...initProdcus]);
+
+    useEffect(() => {
+
+        setInterval(() => {
+            data.map(p => {
+                if (products.length === 0) {
+                     setProducts(p)   
+                }else { 
+                    setProducts([...products, p])
+                }
+            })
+        }, 1000);
+    })
+
+
+    return (
+        <div>
+           {!products ? null : products.map(p => {
+               return (<div>
+                   <span>{p.name}</span>
+                   <span>{p.id}</span>
+                   <span>{p.weight}</span>
+                    <input type='checkbox' /> 
+               </div> ) 
+           })}     
+        </div>
+    )
+}
+
+
+
+
+// class Products extends React.Component { 
+
+//     state = { 
+//         products : Array(1000).fill({})
+//     }
+
+//     componentDidMount = () => { 
+//             this.state.products.map(p => {
+
+//             })
+//     }
+//     render(){
+//         return (
+//             <div>
+
+//             </div>
+//         )
+//     }
+// }
+
+
+
+export default Products; 
---
+++ src/components/data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        name :'test' , 
+        id :1 ,
+        weight :1.3
+    },
+    {
+        name :'test2' , 
+        id :2 ,
+        weight :1.3
+    },{
+        name :'test3' , 
+        id :3 ,
+        weight :1.3
+    },{
+        name :'test4' , 
+        id :4 ,
+        weight :1.3
+    },    
+]
